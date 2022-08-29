151322ddda347c8785ccefb75eb95eb21331b57f
Marius Wallin <post@mariuswallin.no>
2022-08-29 11:29:59 +0200

    add final code

--- uin_oppgave_2/main.js
+++ uin_oppgave_2/main.js
@@ -26,4 +26,90 @@ const wordList = [
   "Dra",
 ];
 
+// Hente ut alle elementer jeg trenger
 
+// Koble til lytter på test knapp
+// Lage funksjon som henter ut tilfeldig ord
+
+
+let didFinish = false;
+const spans = document.querySelectorAll("span");
+const inputs = document.querySelectorAll("input");
+const test = document.getElementById("test");
+const styles = getComputedStyle(document.documentElement);
+
+const random = () => {
+  return Math.floor(Math.random() * wordList.length);
+};
+
+const addWords = () => {
+  [...spans].forEach((span) => (span.innerHTML = wordList[random()]));
+};
+
+const sortWords = () => {
+  const words = [...spans].map((span) => span.innerHTML);
+  return words.sort();
+};
+
+const canValidate = () => {
+  const numbers = [...inputs].map((input) => input.value);
+  const uniqueNumbers = [...new Set(numbers)];
+  return (
+    uniqueNumbers?.filter(
+      (value) =>
+        value && Number(value) && Number(value) > 0 && Number(value) < 5
+    )?.length === 4
+  );
+};
+
+const updateBackgroundColor = (color, initialText, resolvedText) => {
+  test.style.backgroundColor = styles.getPropertyValue(color);
+  test.style.color = styles.getPropertyValue("--white");
+  test.innerHTML = resolvedText;
+
+  setTimeout(() => {
+    test.style.backgroundColor = styles.getPropertyValue("--white");
+    test.style.color = styles.getPropertyValue("--black");
+    test.innerHTML = initialText;
+  }, 1000);
+};
+
+const startNewRound = () => {
+  test.style.backgroundColor = "#ffffff";
+  test.innerHTML = "Test";
+  Array.from(inputs).forEach((input) => (input.value = ""));
+  addWords();
+};
+
+const validate = () => {
+  if (didFinish) {
+    startNewRound();
+    didFinish = false;
+    return;
+  }
+  const sortedWords = sortWords();
+  if (canValidate()) {
+    const guesses = [...inputs].map((input) => {
+      return {
+        value: input.value,
+        word: input.nextElementSibling.innerHTML,
+      };
+    });
+    if (
+      guesses.filter(
+        (guess) => sortedWords[Number(guess.value) - 1] === guess.word
+      )?.length === 4
+    ) {
+      updateBackgroundColor("--green", "Ny runde", "Supert");
+      didFinish = true;
+    } else {
+      updateBackgroundColor("--red", "Prøv igjen", "Feil");
+    }
+  } else {
+    updateBackgroundColor("--purple", "Prøv igjen", "Mangler verdier");
+  }
+};
+
+test.addEventListener("click", validate);
+
+addWords();
