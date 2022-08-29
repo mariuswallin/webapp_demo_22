0f79d678cd45fdcf28a6660af31b99d3639f8aa9
Marius Wallin <post@mariuswallin.no>
2022-08-29 20:57:16 +0200

    add comments

--- currency_converter/index.js
+++ currency_converter/index.js
@@ -1,11 +1,14 @@
 const prompt = require('prompt-sync')()
 
+// Lager et objekt for å holde på konverteringsratene
 const rates = {
   dkk: 1.34,
   eur: 10,
   usd: 8,
 };
 
+// Lager et objekt for å holde på oversettelsene
+// Gjør det mulig å "slå opp" i denne når vi har valgt et språk
 const translations = {
   no: {
     welcomeMessage: "Norsk (no)",
@@ -27,6 +30,7 @@ const translations = {
   },
 };
 
+// Bruker translations objektet til å lage en velkomstbeskjed
 function getWelcomeMessage(translations) {
   let message = "";
   for (const language of Object.values(translations)) {
@@ -35,19 +39,25 @@ function getWelcomeMessage(translations) {
   return message;
 }
 
+// Henter ut meldingen som skal vises når vi skal skrive inn en verdi (no eller en)
 function getValueMessage(translation) {
   return translation["valueMessage"];
 }
 
+// Gjennomførerer konverteringen og runder av til 2 desimaler
 function handleConvertion(rate, value, decimals = 2) {
   return Number(Number(value) / Number(rate)).toFixed(decimals);
 }
 
+// Lager beskjeden som omhandler valg av valuta
 function getCurrencyMessage(currencies) {
   let message = "";
 
+  // Henter ut nøkkelen og verdiene til i translations[language].currencies
+  // Bruker det til å lage "Velg valuta tekststrengen"
   for (const entry of Object.entries(currencies)) {
-    console.log(entry)
+    // For hver iterasjon får vi eks. ['dkk', 'Danish krone']
+    // Deconstructer ut verdiene slik at short blir 'dkk' og long blir 'Danish krone'
     const [short, long] = entry;
     message += `${long}(${short}) `;
   }
@@ -55,14 +65,22 @@ function getCurrencyMessage(currencies) {
   return message;
 }
 
+// Kobler alt sammen
 function start(translations, rates) {
+  // Setter språk
   const language = prompt(getWelcomeMessage(translations));
+  // Henter ut oversettelsene til språket som ble valgt
   const translation = translations[language];
+  // Henter ut oversettelsene for currencies for gitt språk
   const currencies = translation["currencies"];
+  // Henter ut valueMessage for gitt språk
   const valueMessage = getValueMessage(translation);
+  // Promter om å sette en verdi
   const value = prompt(valueMessage);
+  // Lager currencyMessage for et gitt språk
   const currencyMessage = getCurrencyMessage(currencies);
   const currency = prompt(currencyMessage);
+  // Returnerer resultatet av konverteringen
   return handleConvertion(rates[currency], value);
 }
 
--- uin_oppgave_2/main.js
+++ uin_oppgave_2/main.js
@@ -26,31 +26,40 @@ const wordList = [
   "Dra",
 ];
 
-// Hente ut alle elementer jeg trenger
-
-// Koble til lytter på test knapp
-// Lage funksjon som henter ut tilfeldig ord
-
+// Setter didFinish til false til å begynne med 
+// switches til true når vi har løst sorteringen
 
 let didFinish = false;
+
+// Henter alle elemente vi trenger
 const spans = document.querySelectorAll("span");
 const inputs = document.querySelectorAll("input");
 const test = document.getElementById("test");
+
+// Henter ut stilene fra stylesheets. Slik at vi kan hente ut :root {} fargene
 const styles = getComputedStyle(document.documentElement);
 
+// Funksjon som gir oss et random tall mellom 0 og lengden på ordlisten
 const random = () => {
   return Math.floor(Math.random() * wordList.length);
 };
 
+
 const addWords = () => {
+  // Går igjennom alle span-nodene. Til hver node sin tekst hentes det
+  // ut et random ord fra ordlisten. 
   [...spans].forEach((span) => (span.innerHTML = wordList[random()]));
 };
 
+// Funksjon vi kan bruke for få rett sortering av ordene i span`ene
+// Kan da denne sorterte listen for å sjekke at vi har sortert riktig
 const sortWords = () => {
   const words = [...spans].map((span) => span.innerHTML);
   return words.sort();
 };
 
+// Funksjon brukt for å sjekke at verdiene i input oppfyller minimumskravene
+// Sjekker at vi har en verdi, at det er et tall og tallet er større enn 0 og mindre enn 5
 const canValidate = () => {
   const numbers = [...inputs].map((input) => input.value);
   const uniqueNumbers = [...new Set(numbers)];
@@ -62,11 +71,14 @@ const canValidate = () => {
   );
 };
 
+// Funksjon brukt for å endre bakgrunnen på knappen
+// Kan da bruke variablene som er på :root {} i CSSen
 const updateBackgroundColor = (color, initialText, resolvedText) => {
   test.style.backgroundColor = styles.getPropertyValue(color);
   test.style.color = styles.getPropertyValue("--white");
   test.innerHTML = resolvedText;
 
+  // Nullstiller fargendringen etter 1 sekund (1000ms)
   setTimeout(() => {
     test.style.backgroundColor = styles.getPropertyValue("--white");
     test.style.color = styles.getPropertyValue("--black");
@@ -74,6 +86,7 @@ const updateBackgroundColor = (color, initialText, resolvedText) => {
   }, 1000);
 };
 
+// Starter ny runde med nye ord
 const startNewRound = () => {
   test.style.backgroundColor = "#ffffff";
   test.innerHTML = "Test";
@@ -82,25 +95,37 @@ const startNewRound = () => {
 };
 
 const validate = () => {
+  // Sjekker om vi har klart sorteringen
+  // Hvis tilfelle starter ny runde og didFinish blir false igjen
   if (didFinish) {
     startNewRound();
     didFinish = false;
     return;
   }
   const sortedWords = sortWords();
+  // Sjekker om input-verdiene oppfyller kravene
   if (canValidate()) {
+    // Henter ut verdiene i input og teksten for "nextElementSibling"
+    // nextElementSibling er søsteren til <input /> altså <span></span>
     const guesses = [...inputs].map((input) => {
       return {
         value: input.value,
         word: input.nextElementSibling.innerHTML,
       };
     });
+    // Sjekker om rekkefølgene på sorteringen er korrekt
+    // Bruker den sorterte listen og sjekker om den sammenfaller med
+    // det vi har skrevet input
+    // For at sorteringen er riktig må filteret under gi en lengde på 4
+    // som vil si at alle verdiene er sortert riktig
     if (
       guesses.filter(
         (guess) => sortedWords[Number(guess.value) - 1] === guess.word
       )?.length === 4
     ) {
       updateBackgroundColor("--green", "Ny runde", "Supert");
+      // Hvis sorteringen er riktig flipp didFinish til true
+      // Gjør at neste gang vi trykker knappen så starter ny runde (se i starten av denne funksjonen)
       didFinish = true;
     } else {
       updateBackgroundColor("--red", "Prøv igjen", "Feil");
@@ -110,6 +135,7 @@ const validate = () => {
   }
 };
 
+// Lytter til klikk på knappen
 test.addEventListener("click", validate);
 
 addWords();
