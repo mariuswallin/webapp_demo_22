88b6b42f52a592848fc305b2e07adb83ef08779c
mariuswallin <post@mariuswallin.no>
2022-09-20 19:03:40 +0200

    fix(text): fix text

--- promises/promise_all.js
+++ promises/promise_all.js
@@ -15,8 +15,8 @@ const demo3 = (id) =>
     resolve("Demo 3");
   });
 
-const main_no_error = async () => {
-  const list = [..."abcde"];
+const mainZero = async () => {
+  const list = [..."abc"];
   try {
     const data = list.map(async (l) => {
       const res1 = await demo2;
@@ -25,38 +25,34 @@ const main_no_error = async () => {
       return res1, res2;
     });
 
-    console.log("One data", data);
+    console.log("Zero Data", data);
     const result = await Promise.all(data);
-    console.log("One result", result);
+    console.log("Zero Result", result);
   } catch (error) {
-    console.log("Error", error);
+    console.log("Zero Error", error);
   }
 };
 
-const main_error = async () => {
-  const list = [..."abcde"];
+const mainOne = async () => {
+  const list = [..."abc"];
   try {
     const data = list.map(async (l) => {
       const res1 = await demo2;
       const res2 = await demo3(res1);
       const res3 = await demo();
       return res1, res2, res3;
-
-      // HVis vi returnerer array her [res1, res2] så får vi arrays med array
-      // Skjer fordi vi får en liste med promises, som når vi resolver så er det lister i denne
-      // Dette da vi har async i map over
     });
-    console.log("Two data", data);
-    // Hvis data kaster error og vi ikke resolver de så får vi ikke catchet feilen
-    // Må da ha try catch inne i mappen
-    // const result = await Promise.all(data);
-    // console.log("Two result", result);
+    console.log("One Data", data);
+    // Hvis noe i data kaster error og vi ikke resolver den med Promise.all får vi ikke catchet feilen
+    // Må da ha try catch inne i .map()
+    const result = await Promise.all(data);
+    console.log("One Result", result);
   } catch (error) {
-    console.log("Error", error);
+    console.log("One Error", error);
   }
 };
 
-const main_error_catch = async () => {
+const mainTwo = async () => {
   const list = [..."abcde"];
   try {
     const data = list.map(async (l) => {
@@ -64,21 +60,22 @@ const main_error_catch = async () => {
         const res1 = await demo2;
         const res2 = await demo3(res1);
         const res3 = await demo();
+        // Hvis vi returnerer array her [res1, res2] får vi arrays med array som resultat.
+        // Dette fordi vi får en liste med promises tilbake fra map.
+        // Når vi resolver verdiene i listen får vi tilgang til [res1, res2]
+        // Dette skjer da vi har async i map over, som gjør at .map gir promises tilbake
         return res1, res2, res3;
       } catch (error) {
-        console.log("ERROR");
+        console.log("Two Inner Error");
       }
-      // HVis vi returnerer array her [res1, res2] så får vi arrays med array
-      // Skjer fordi vi får en liste med promises, som når vi resolver så er det lister i denne
-      // Dette da vi har async i map over
     });
-    console.log("Two data", data);
-    // Hvis data kaster error og vi ikke resolver de så får vi ikke catchet feilen
-    // Må da ha try catch inne i mappen
-    // const result = await Promise.all(data);
-    // console.log("Two result", result);
+    console.log("Two Data", data);
+    // Hvis noe i data kaster error og vi ikke resolver den med Promise.all får vi ikke catchet feilen
+    // Må da ha try catch inne i .map()
+    const result = await Promise.all(data);
+    console.log("Two Result", result);
   } catch (error) {
-    console.log("Error", error);
+    console.log("Two Error", error);
   }
 };
 
@@ -90,21 +87,22 @@ const mainThree = async () => {
       const res1 = demo2;
       const res2 = demo3();
       const res3 = demo();
+      // Her kunne vi hatt [res1, res2, res3] 
+      // Da vi ikke bruker async får vi en liste med promises
+      // Ikke en liste med promises som igjen inneholder arrays
+      // Det gjør at vi kan løse opp verdiene
       return res1, res2, res3;
     });
-
-    // Her får en liste med arrays som igjen er promises
-    // Som gjør at vi kan løse opp
     console.log("Three Data", data);
     const result = await Promise.all(data);
-    console.log("Three result", result);
+    console.log("Three Result", result);
   } catch (error) {
-    console.log("Error", error);
+    console.log("Three Error", error);
   }
 };
 
 
-const func = async () => {
+const mainFour = async () => {
   for (const value of [..."abc"]) {
     try {
       await demo();
@@ -114,27 +112,29 @@ const func = async () => {
   }
 }
 
-// func()
-// main_no_error();
-// main_error();
-// main_error_catch();
+// mainZero();
+// mainOne();
+// mainTwo();
 // mainThree();
+// mainFour()
 
 /**
  * 
  * Selv om vi bruker Promise.resolve() så vil den først faile når alle promiser kommer tilbake
- * Så om 5 api kall går igjennom, men 1 ikke går så catcher vi feilen, men det vil si at f.eks 5 artikler har blit laget
- * Må da alternativt ha en "opprydding" som sletter det vi laget om vi ønsker at alt skal gå igjennom
+ * Så om 5 api kall går igjennom, men 1 ikke går så catcher vi feilen, men det vil si at f.eks 5 har blitt laget
+ * Må da ha en "opprydding" som sletter det vi laget om vi ønsker at alt eller ingen ting skal gå igjennom
  * 
  */
 
 /**
- * Obs. Når vi try/catch inne i en .map så må vi returnere feilen og ikke throw videre om resultatet av mappen ikke blir resolved seinere
- * 
+ * Obs. Når vi bruker try/catch inne i en .map må vi returnere feilen og ikke kaste feilen videre. Dette gjelder om resultatet av mappen ikke blir resolved seinere med Promise.all().
  * 
  */
 
 /**
+ * 
+ * Noen fine artikler
+ * 
  * https://hackernoon.com/changing-asyncawait-to-promisesall-to-speed-up-api-calls-in-nodejs
  * https://dev.to/raviojha/javascript-making-multiple-api-calls-the-right-way-2b29
  * 
